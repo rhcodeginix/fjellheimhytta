22b94a880bad924b2eff710f47b82746e55a4f06
Drashti Savani <rhshukla124@gmail.com>
2025-06-11 12:37:59 +0530

    add some change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -237,7 +237,7 @@ const index = () => {
         }}
       >
         {loading && <Loader />}
-      </div>{" "}
+      </div>
       <div className="relative">
         <MainSection />
         <HowItWorks />
