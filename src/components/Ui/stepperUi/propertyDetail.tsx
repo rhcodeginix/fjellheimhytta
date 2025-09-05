c503d9665873106c606e2b08e1570fd3f17317c4
Drashti Savani <rhshukla124@gmail.com>
2025-09-05 12:01:44 +0530

    add map zoom

--- src/components/Ui/stepperUi/propertyDetail.tsx
+++ src/components/Ui/stepperUi/propertyDetail.tsx
@@ -53,6 +53,7 @@ const PropertyDetail: React.FC<any> = ({
                     coordinates={
                       lamdaDataFromApi?.coordinates?.convertedCoordinates
                     }
+                    zoom={18}
                   />
                   // <NorkartMap
                   //   coordinates={
